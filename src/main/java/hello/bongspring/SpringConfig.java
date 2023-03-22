520dcec80aced3789d3a4782e2b284e11c2c4d1a
bongDev <bongdev29@gmail.com>
2023-03-22 16:42:30 +0900

    구버전 JDBC 연결 예제

--- src/main/java/hello/bongspring/SpringConfig.java
+++ src/main/java/hello/bongspring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.bongspring;
 
+import hello.bongspring.repository.JdbcMemberRepository;
 import hello.bongspring.repository.MemberRepository;
-import hello.bongspring.repository.MemoryMemberRepository;
 import hello.bongspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
 
@@ -17,6 +28,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
     }
 }
